b251747f9badc0e8868e89ef172df54b5e2b0d2b
uliasuprun <uliasuprun88@gmail.com>
2023-03-19 15:57:18 +0200

    added start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import React from "react";
+
+import Feedback from './Feedback';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Feedback />
     </div>
   );
 };
